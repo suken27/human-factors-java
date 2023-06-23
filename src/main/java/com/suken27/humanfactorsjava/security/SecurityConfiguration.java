56b858692753bff46c19b580cd84b13c2cac5e82
suken27 <suken2727@gmail.com>
2023-06-23 14:38:40 +0200

    HttpBasic configuration.

--- src/main/java/com/suken27/humanfactorsjava/security/SecurityConfiguration.java
+++ src/main/java/com/suken27/humanfactorsjava/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.suken27.humanfactorsjava.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +14,7 @@ public class SecurityConfiguration{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
+        http.httpBasic(Customizer.withDefaults());
         http.authorizeHttpRequests(authorizeHttpRequests ->
         authorizeHttpRequests.requestMatchers("/login", "/signup").permitAll().anyRequest().authenticated());
         return http.build();
